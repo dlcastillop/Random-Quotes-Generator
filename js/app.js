177ad04514d1a6981324b295ce676e82fee2c40d
Daniel Castillo <dlcastillo3015@gmail.com>
2022-03-05 15:31:16 -0500

    El boton realiza la solicitud de la API y muestra frase-autor

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+async function getapi() {
+  const response = await fetch("https://api.quotable.io/random");
+  let datos = await response.json();
+  let frase = datos["content"];
+  let autor = datos["author"];
+  document.getElementsByTagName("h1")[0].textContent = frase;
+  document.getElementsByTagName("h2")[0].textContent = autor;
+}
 
+getapi();
+
+document.getElementById("boton").addEventListener("click", getapi);
